765a0b4ffa97029aeaafb6ef22e49358dc9eb51a
Tony Fountaine <william.anzac@gmail.com>
2016-06-03 23:19:41 +1200

    Initial commit

---
+++ src/main/java/nz/co/anzac/moneymanager/App.java
@@ -0,0 +1,13 @@
+package nz.co.anzac.moneymanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
